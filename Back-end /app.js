6e3b905b3e78a323ef70d0d50333dab0e03f0a4b
Rebekah <bekahramirez08@gmail.com>
2026-04-08 16:48:49 -0700

    Connected code to Database

--- Back-end /app.js
+++ Back-end /app.js
@@ -1,6 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+/*to test if connected to database */ 
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Database connected:', res.rows[0]);
+  }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
