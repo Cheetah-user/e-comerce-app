ce2a45429ff7101688f2c56a1e19934060c5475d
Rebekah <bekahramirez08@gmail.com>
2026-04-13 11:03:56 -0700

    added registration route

--- Back-end /app.js
+++ Back-end /app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,17 +13,64 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-/*to test if connected to database */ 
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Database connection error:', err);
-  } else {
-    console.log('Database connected:', res.rows[0]);
-  }
-});
+app.use(express.json());
 
+/*Basic starting route*/
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+/*Category routes */
+app.get('/categories', (req, res) => {
+  res.send();
+});
+
+/*Products routes*/
+app.get('/products', (req, res) => {
+
+});
+
+/*Orders routes*/
+app.get('/orders', (req, res) => {
+
+});
+
+/*Payments routes */
+app.get('/payments', (req, res) => {
+
+});
+
+/*Customers routes*/
+app.get('/customers', (req, res,) => {
+  res.send();
+});
+
+/*path lets the user register with name, email, and a password(which is encrypted) and sends a message if successful */
+app.post('/customers/register', async (req, res) => {
+    const {name, email, password} = req.body;
+    try{
+       const saltRounds = 10;
+       const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+       await pool.query(
+        'INSERT INTO customers (name, email, password) VALUES ($1, $2, $3)',
+        [name, email, hashedPassword]
+       );
+      res.send('User registered!');
+    }catch(err){
+       if(err.code === '23505'){
+        return res.status(400).send('Error, user with that email already exists')
+       }
+       res.status(500).send('Server Error');
+    }
+});
+
+/* */
+
+
+/*Reviews routes*/
+app.get('/reviews', (req, res) => {
+
 });
 
 app.listen(port, () => {
