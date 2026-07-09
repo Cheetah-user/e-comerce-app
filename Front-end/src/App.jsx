49cc8819a5c6b2f2ad68fad76ba98621a8e4921c
Rebekah <bekahramirez08@gmail.com>
2026-07-09 12:19:29 -0700

    Start writting routes

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,8 +1,17 @@
 
 import './App.css'
+import { Routes, Route } from 'react-router' 
 
 function App() {
- 
+  return (
+   <Routes>
+      <Route path='/' element={<h1>Home page</h1>}/>
+      <Route path='/login' element={<h1>Login page</h1>}/>
+      <Route path='/register' element={<h1>Register page</h1>}/>
+      <Route path='/products' element={<h1>Products page</h1>}/>
+      <Route path='/categories' element={<h1>Categories page</h1>}/>
+   </Routes>
+  );
 }
 
-export default App
+export default App;
