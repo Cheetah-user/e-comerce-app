941c6ef4dd49a8c2a08296e542acf8eb9dd0e5d5
Rebekah <bekahramirez08@gmail.com>
2026-07-13 11:04:27 -0700

    Creat register form

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import { Routes, Route } from 'react-router' 
+import Register from './pages/register';
 
 function App() {
   return (
    <Routes>
       <Route path='/' element={<h1>Home page</h1>}/>
       <Route path='/login' element={<h1>Login page</h1>}/>
-      <Route path='/register' element={<h1>Register page</h1>}/>
+      <Route path='/registration' element={<Register/>}/>
       <Route path='/products' element={<h1>Products page</h1>}/>
       <Route path='/categories' element={<h1>Categories page</h1>}/>
    </Routes>
---
+++ Front-end/src/pages/register.jsx
@@ -0,0 +1,60 @@
+import React, {useState} from "react";
+
+function Register() {
+    const [formData, setFormData] = useState({username: '', email: '', password: ''});
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+    // Handle input changes
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+    // handle form submissions 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setMessage('');
+        setError(false);
+        try {
+            const response = await fetch('http://localhost:3000/register', {
+               method: 'POST',
+               headers: {
+                'Content-Type': 'application/json'
+               },
+               body: JSON.stringify(formData),
+            });
+
+            const data = await response.text(); 
+            //checks if the response is ok
+            if (!response.ok){
+                setError(true);
+                setMessage(data); // Displays the error message: "Username already exists" or "Email already exists"
+                return;
+            }
+
+            setMessage(data); //Displays the success message: "User registered sucessfully"
+            setFormData({username: '', email: '', password: ''}); // Resets the form fields
+        }catch(err){
+            setError(true);
+            setMessage('Failed to connect to the server');
+        }
+    };
+
+    return (
+        <div style={{maxWidth: '400px', margin: 'auto', padding: '20px'}}>
+            <h2> Create an Account</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="username" placeholder="Username" required value={formData.username} onChange={handleChange}/>
+                <input type="email" name="email" placeholder="Email" required value={formData.email} onChange={handleChange} />
+                <input type="password" name="password" placeholder="Password" required value={formData.password} onChange={handleChange}/>
+                <button type="submit">Register</button>
+            </form>
+
+            {message && (
+                <p style={{color: error ? 'red': 'green', marginTop: '10px' }}>
+                    {message}
+                </p>
+            )}
+        </div>
+    )
+}
+
+export default Register;
